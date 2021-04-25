43a41ba2fa216961c591146eeea163df20daf059
Anthony <spitfyre03@gmail.com>
2021-04-25 00:28:00 -0400

    Add EntityCucco Class
    Add a basic class extending chickens

---
+++ src/main/java/zeldaswordskills/entity/passive/EntityCucco.java
@@ -0,0 +1,12 @@
+package zeldaswordskills.entity.passive;
+
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.world.World;
+
+public class EntityCucco extends EntityChicken {
+
+	public EntityCucco(World worldIn) {
+		super(worldIn);
+	}
+
+}
